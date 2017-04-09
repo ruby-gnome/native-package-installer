a988cdf465a7b6d74044ec192dd1f58096cee0f3
Kouhei Sutou <kou@clear-code.com>
2017-04-10 00:03:03 +0900

    Add installer

---
+++ lib/native-package/platform/alt-linux.rb
@@ -0,0 +1,32 @@
+# Copyright (C) 2017  Kouhei Sutou <kou@cozmixng.org>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module NativePackage
+  module Platform
+    class ALTLinux
+      Platform.register(self)
+
+      class << self
+        def current_platform?
+          File.exist?("/etc/altlinux-release")
+        end
+      end
+
+      def package(spec)
+        spec[:alt_linux]
+      end
+    end
+  end
+end
