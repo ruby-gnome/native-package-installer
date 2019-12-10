36db79b1551bcc1910365462b196cbd93469fce6
Sutou Kouhei <kou@clear-code.com>
2019-12-10 14:57:09 +0900

    ubuntu: ensure requiring debian

--- lib/native-package-installer/platform/ubuntu.rb
+++ lib/native-package-installer/platform/ubuntu.rb
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require "native-package-installer/platform/debian"
+
 class NativePackageInstaller
   module Platform
     class Ubuntu < Debian
