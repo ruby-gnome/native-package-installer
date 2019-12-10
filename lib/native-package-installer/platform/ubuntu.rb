407bcbbcf4e3f0b61051d2cc4020e6868cbdca40
Sutou Kouhei <kou@clear-code.com>
2019-12-10 14:58:59 +0900

    Ruby-GNOME2 -> Ruby-GNOME

--- lib/native-package-installer/platform/ubuntu.rb
+++ lib/native-package-installer/platform/ubuntu.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013  Ruby-GNOME2 Project Team
+# Copyright (C) 2019  Ruby-GNOME Project Team
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
