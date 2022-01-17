dd372e642652e56ba29a9df9818896cb09f51a5a
Sutou Kouhei <kou@clear-code.com>
2022-01-18 05:57:22 +0900

    test: specify test directory explicitly

--- test/run-test.rb
+++ test/run-test.rb
@@ -20,4 +20,10 @@ $VERBOSE = true
 require "test-unit"
 require "test/unit/rr"
 
-exit(Test::Unit::AutoRunner.run(true))
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+lib_dir = File.join(base_dir, "lib")
+test_dir = File.join(base_dir, "test")
+
+$LOAD_PATH.unshift(lib_dir)
+
+exit(Test::Unit::AutoRunner.run(true, test_dir))
